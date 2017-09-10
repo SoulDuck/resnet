ca7f602b589a96f2aa856ef97d1b6512f5029ed8
kimseongjung <plznw4me@naver.com>
2017-09-11 00:29:36 +0900

    add eval : default eval dataset--> eval

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ FLAGS=tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('dataset' , 'cifar10' , 'cifar-10 or cifar-100')
 tf.app.flags.DEFINE_string('mode', 'eval','train or eval')
 tf.app.flags.DEFINE_string('train_data_path','cifar-10/data_batch*','Filepattern for training data')
-tf.app.flags.DEFINE_string('eval_data_path' , '' , 'Filepatter for eval data')
+tf.app.flags.DEFINE_string('eval_data_path' , 'cifar-10/test_batch.bin' , 'Filepatter for eval data')
 tf.app.flags.DEFINE_integer('image_size', 32 , 'Image side length')
 tf.app.flags.DEFINE_string('train_dir','./output/train','Directory to keep training outputs')
 tf.app.flags.DEFINE_string('eval_dir','./output/eval', 'Directory to keep eval outputs')
