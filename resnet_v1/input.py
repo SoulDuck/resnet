052d4c8cd59376f659200d02a78a7c083b6ee1fb
kimseongjung <plznw4me@naver.com>
2017-09-12 18:26:13 +0900

    add resnet_v1 (include unit 4 , 4_1 , 4_2)

---
+++ resnet_v1/input.py
@@ -0,0 +1,214 @@
+import tensorflow as tf
+
+
+def _read_one_example( tfrecord_path , resize=(300,300)):
+    tfrecord_paths=tf.gfile.Glob('./sample_tfrecord/*.tfrecord')
+    print tfrecord_paths
+    filename_queue = tf.train.string_input_producer(tfrecord_paths , num_epochs=10)
+    reader = tf.TFRecordReader()
+    _ , serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example,
+      # Defaults are not specified since both keys are required.
+      features={
+        'height': tf.FixedLenFeature([], tf.int64),
+        'width': tf.FixedLenFeature([], tf.int64),
+        'raw_image': tf.FixedLenFeature([], tf.string),
+        'label' : tf.FixedLenFeature([] , tf.int64),
+        'filename': tf.FixedLenFeature([], tf.string)
+        })
+    image = tf.decode_raw(features['raw_image'], tf.uint8)
+    height= tf.cast(features['height'] , tf.int32)
+    width = tf.cast(features['width'] , tf.int32)
+    label = tf.cast(features['label'] , tf.int32)
+    filename = tf.cast(features['filename'] , tf.string)
+
+    image_shape = tf.stack([height , width , 3 ])
+    image=tf.reshape(image ,  image_shape)
+    if not resize == None :
+        resize_height , resize_width  = resize
+        image_size_const = tf.constant((resize_height , resize_width , 3) , dtype = tf.int32)
+        image = tf.image.resize_image_with_crop_or_pad(image=image,
+                                               target_height=resize_height,
+                                               target_width=resize_width)
+    #images  = tf.train.shuffle_batch([image ] , batch_size =batch_size  , capacity =30 ,num_threads=3 , min_after_dequeue=10)
+    return image,label,filename
+
+
+def build_input(dataset , data_path , batch_size , mode):
+    if dataset == 'cifar10':
+        label_bytes = 1
+        label_offset = 0 #label offset =0 main cifar10
+        n_classes = 10
+        depth =3
+        image_size=32
+    elif dataset == 'cifar100':
+        label_bytes = 1
+        label_offset = 1 #label offset =1 mena cifar 100
+        n_classes = 100
+        depth =3
+        image_size = 32
+    elif dataset == 'fundus_300x300':
+        n_classes =2
+        depth =3
+        image_size = 224
+    else:
+        raise ValueError('Not supported dataset')
+
+
+    if dataset == 'cifar10' or dataset == 'cifar100':
+
+        image_bytes = image_size * image_size * depth
+        record_bytes = label_bytes + label_offset + image_bytes
+        print data_path
+        data_files = tf.gfile.Glob(data_path)
+        print 'data files : ', data_files
+        file_queue = tf.train.string_input_producer(data_files , shuffle=True)
+        reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+        _ , value = reader.read(file_queue)
+
+        #convoert these examples to dense labels , and processed images
+        record = tf.reshape(tf.decode_raw(value , tf.uint8),[record_bytes])
+        label = tf.cast(tf.slice(record , [label_offset] , [label_bytes]) , tf.int32)
+
+        #Convert from string to [depth * height * width ] to [depth , height , width]
+        depth_major = tf.reshape(tf.slice(record , [label_offset + label_bytes] , [image_bytes]) , [depth , image_size , image_size])
+
+        #Convert from [ch , h , w ] to [h,w,ch]
+        image = tf.cast(tf.transpose(depth_major , [1,2,0]) , dtype =  tf.float32)
+    elif dataset=='fundus_300x300':
+        image , label  =_read_one_example('./sample_tfrecord/*.record.out', (image_size,image_size))
+
+
+
+    if mode == 'train':
+        image = tf.image.resize_image_with_crop_or_pad(image , image_size+4 , image_size+4)
+        image = tf.random_crop(image , [image_size , image_size ,3])
+        image = tf.image.random_flip_left_right(image)
+        image = tf.image.random_flip_up_down(image)
+        # Brightness / saturatio / constrast provides samll gains 2%~5% on cifar
+
+        image = tf.image.random_brightness(image , max_delta=63. / 255.)
+        image = tf.image.random_saturation(image , lower=0.5 , upper=1.8)
+        image = tf.image.per_image_standardization(image)
+
+        example_queue = tf.RandomShuffleQueue(
+            capacity=16 * batch_size,
+            min_after_dequeue=8*batch_size,
+            dtypes = [tf.float32 , tf.int32],
+            shapes = [[image_size , image_size , depth] , [1] ])
+        num_threads=16
+    else:
+        image = tf.image.resize_image_with_crop_or_pad(image, image_size, image_size)
+        image = tf.image.per_image_standardization(image)
+
+        example_queue = tf.FIFOQueue(
+            3*batch_size ,
+            dtypes = [tf.float32 , tf.int32 , ],
+            shapes = [[image_size , image_size , depth] , [1] ,])
+
+        num_threads = 1
+    label=tf.reshape(label , [1])
+    print label
+    example_enqueue_op = example_queue.enqueue([image ,label ])
+    tf.train.add_queue_runner(tf.train.queue_runner.QueueRunner(example_queue , [example_enqueue_op]))
+    # Read 'batch' labels + images from the example queue
+
+
+    images , labels  = example_queue.dequeue_many(batch_size)
+    labels = tf.reshape(labels , [batch_size , 1])
+    indices = tf.reshape(tf.range(0 , batch_size , 1), [batch_size ,1 ])
+    images, labels = example_queue.dequeue_many(batch_size)
+
+
+    tf.summary.histogram('labels' , labels)
+    labels = tf.sparse_to_dense(
+        tf.concat(values=[indices , labels] ,axis=1),
+        [batch_size , n_classes] , 1.0 , 0.0)
+
+    assert len(images.get_shape()) == 4
+    assert images.get_shape()[0] == batch_size
+    assert images.get_shape()[-1] == 3
+    assert len(labels.get_shape()) ==2 , len(labels.get_shape())
+    assert labels.get_shape()[0] == batch_size , labels.get_shape()[0]
+    assert labels.get_shape()[1] == n_classes  , labels.get_shape()[1]
+
+
+
+    tf.summary.image('images' , images)
+    return images , labels
+
+
+if '__main__' == __name__:
+
+
+    image_size=300
+
+    label_bytes = 1
+    label_offset = 1 #label offset =0 main cifar10
+    n_classes = 10
+
+    depth =3
+    batch_size=128
+    image_bytes = image_size * image_size * depth
+
+    record_bytes = label_bytes + label_offset + image_bytes
+    print record_bytes
+    print image_bytes
+    print label_bytes
+    print label_offset
+    data_path='cifar-10/data_batch*'
+    data_files = tf.gfile.Glob(data_path)
+    print data_files[0:2]
+    file_queue = tf.train.string_input_producer(data_files[0:2] , shuffle=True)
+    reader = tf.FixedLengthRecordReader(record_bytes=record_bytes)
+
+
+    _ , value = reader.read(file_queue)
+    record = tf.reshape(tf.decode_raw(value, tf.uint8), [record_bytes])
+    label = tf.cast(tf.slice(record, [label_offset], [label_bytes]), tf.int32)
+    depth_major = tf.reshape(tf.slice(record , [label_offset + label_bytes] , [image_bytes]) , [depth , image_size , image_size])
+    init=tf.global_variables_initializer()
+    sess=tf.Session()
+    sess.run(init)
+
+    coord= tf.train.Coordinator()
+    threads=tf.train.start_queue_runners(sess= sess ,coord = coord)
+
+    try:
+        count=0
+        while not coord.should_stop():
+            print len(sess.run(value))
+            count += 1
+    except tf.errors.OutOfRangeError:
+        print('Done training -- epoch limit reached')
+    finally:
+        print count
+        # When done, ask the threads to stop.
+        coord.request_stop()
+    coord.join(threads)
+    sess.close()
+
+
+
+    """
+    #Convert from [ch , h , w ] to [h,w,ch]
+    #image = tf.cast(tf.transpose(depth_major , [1,2,0]) , dtype =  tf.float32)
+    example_queue = tf.FIFOQueue(
+        3*batch_size ,
+        dtypes = [tf.float32 , tf.int32],
+        shapes = [[image_size , image_size , depth] , [1]])
+    num_threads = 1
+    example_enqueue_op = example_queue.enqueue([image ,label])
+    tf.train.add_queue_runner(tf.train.queue_runner.QueueRunner(example_queue , [example_enqueue_op]))
+    images, labels = example_queue.dequeue_many(batch_size)
+    indices = tf.reshape(tf.range(0 , batch_size , 1), [batch_size ,1 ])
+
+    indices = tf.sparse_to_dense(
+        tf.concat(values=[indices , labels] ,axis=1),
+        [batch_size , n_classes] , 1.0 , 0.0 )
+    print record
+    print label
+    print value
+    print image
+    print indices
+    """
---
+++ resnet_v1/model.py
@@ -0,0 +1,294 @@
+#
+"""ResNet model.
+Related papers:
+https://arxiv.org/pdf/1603.05027v2.pdf
+https://arxiv.org/pdf/1512.03385v1.pdf
+https://arxiv.org/pdf/1605.07146v1.pdf
+"""
+
+from collections import namedtuple
+import input
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.training import moving_averages
+import six
+
+
+
+HParams = namedtuple('HParams',
+                     'batch_size, n_classes, min_lrn_rate, lrn_rate, '
+                     'n_residual_units, use_bottleneck, weight_decay_rate, '
+                     'relu_leakiness, optimizer')
+
+#this param set in main
+
+
+class resnet(object):
+    def __init__(self, hparam, images ,  labels ,mode ):
+        print 'resnet class init'
+        """
+        :param hparam: hyperparameter : learning rate , checkpoint ,
+        :param images: tensor shape rank 4  | original shape
+        :param labels: onehot | cls
+        :param mode: train | eval
+        :return:
+        """
+        self.hps = hparam
+        self._images = images
+        self.label = labels
+        self.mode = mode
+        self._extra_train_ops = []
+
+    def _stride(self ,stride):
+        """
+        Note: if you want more detail strides option . input stride
+        :param stride:
+        :return:
+        """
+        return [1 , stride , stride , 1]
+
+
+    def _conv(self , name , x , k_size , in_ch , out_ch, stride , _padding='SAME'):
+        """
+
+        :param name: tensor name
+        :param x_: input tensor(placeholder)
+        :param filter_size:
+        :param input_filters:
+        :param output_filters:
+        :param strides:
+        :return:
+        """
+        n=k_size * k_size * in_ch
+        k= tf.get_variable('conv_w' ,[k_size , k_size , in_ch , out_ch] , dtype = tf.float32 , \
+                           initializer=tf.random_normal_initializer(stddev=np.sqrt(2.0/n)))
+        return tf.nn.conv2d(x , k , stride , padding=_padding)
+
+    def _act(self , x , actmode='relu'):
+        if actmode =='relu':
+            return tf.nn.relu(x , name='relu')
+        elif actmode == 'leaky_relu':
+            leakiness=0.05 # how about set leakiness randomly?
+            return tf.where(tf.less(x,0.0) , self.hps.relu_leakiness * x , x  , name ='leaky_relu')
+
+        return tf.where(tf.less)
+        """
+        relu | elu | sigmoid |
+        :return:
+        """
+
+    def _batch_norm(self , name , x):
+        """
+
+        :param name:
+        :param x:
+        :return:
+        """
+        p_shape=[x.get_shape()[-1]]
+
+        with tf.variable_scope(name):
+            beta = tf.get_variable(name='beta', shape=p_shape, dtype=tf.float32, \
+                            initializer=tf.constant_initializer(0.0, tf.float32), trainable=False)
+
+
+            gamma = tf.get_variable(name='gamma', shape=p_shape, dtype=tf.float32, \
+                        initializer=tf.constant_initializer(1.0, tf.float32), trainable=False)
+
+
+        if self.mode == 'train':
+
+                    mean , variance =tf.nn.moments(x , [0,1,2] , name = 'momnets')
+                    moving_mean = tf.get_variable('moving_mean',shape=p_shape, dtype=tf.float32 ,\
+                                                  initializer=tf.constant_initializer(0.0 , tf.float32))
+                    moving_variance = tf.get_variable(name='moving_variance', shape=p_shape , dtype=tf.float32 ,\
+                                                      initializer=tf.constant_initializer(1.0 , tf.float32))
+
+                    self._extra_train_ops.append(moving_averages.assign_moving_average(moving_mean , mean , 0.9))
+                    self._extra_train_ops.append(moving_averages.assign_moving_average(moving_variance , variance , 0.9))
+
+        else:
+            mean = tf.get_variable(name = 'moving_mean' ,shape = p_shape , dtype = tf.float32 ,\
+                                  initializer=tf.constant_initializer(0.0 , tf.float32), trainable=False)
+            variance = tf.get_variable(name = 'moving_variance', shape = p_shape , dtype = tf.float32 , \
+                                       initializer=tf.constant_initializer(1.0 , tf.float32) , trainable=False)
+
+            tf.summary.histogram(mean.op.name , mean)
+            tf.summary.histogram(variance.op.name , variance)
+
+        y=tf.nn.batch_normalization(x, mean , variance , beta , gamma , 0.001)
+        y.set_shape(x.get_shape())
+        return y
+
+
+    def _affine(self ,  x ,  out_ch):
+        x=tf.reshape(x,[self.hps.batch_size , -1 ])
+        w=tf.get_variable('fc_w' ,[x.get_shape()[1] , out_ch] , initializer=tf.uniform_unit_scaling_initializer(factor=1.0))
+        b=tf.get_variable('fc_b' ,[out_ch] , initializer=tf.constant_initializer())
+        return tf.nn.xw_plus_b(x,w,b)
+
+
+    def _gap(self , x ):
+        assert x.get_shape().ndims==4
+        return tf.reduce_mean(x , [1,2])
+
+
+    def _residual(self , x , in_ch , out_ch , stride , act_before_residual=False ):
+        if act_before_residual==True:
+            with tf.variable_scope('shared_activation'):
+                x = self._batch_norm('init_bn' , x )
+                x = self._act(x , 'leaky_relu')
+                orig_x = x
+                #print x.get_shape()
+        else:
+            with tf.variable_scope('residual_only_activation'):
+                orig_x = x
+                x = self._batch_norm('init_bn',x)
+                x = self._act(x , 'leaky_relu')
+                #print x.get_shape()
+        with tf.variable_scope('sub1'):
+            x= self._conv('conv1' , x , 3 ,in_ch , out_ch , stride ,)
+        with tf.variable_scope('sub2'):
+            x = self._batch_norm('bn2' , x)
+            x = self._act(x , 'leaky_relu')
+            x= self._conv('conv2' , x , 3 , out_ch , out_ch ,[1,1,1,1])
+        with tf.variable_scope('sub_add'):
+            if in_ch != out_ch:
+                print 'orig_x shape :',orig_x.get_shape()
+                print 'x shape: ',x.get_shape()
+                print 'orig_x shape' , orig_x.get_shape()
+                print 'in_ch : ',in_ch , 'out_ch : ' , out_ch
+                ksize=stride
+                if orig_x.get_shape()[2] ==75 or orig_x.get_shape()[2] == 35 or orig_x.get_shape()[2] == 19:
+                    print '#########',orig_x.get_shape()
+                    orig_x = tf.nn.avg_pool(orig_x, ksize, stride, 'SAME')
+                else:
+                    orig_x = tf.nn.avg_pool(orig_x , ksize, stride , 'VALID')
+
+                orig_x = tf.pad( orig_x, [[0,0],[0,0],[0,0], [(out_ch - in_ch)//2 , (out_ch-in_ch)//2]])
+                print orig_x.get_shape()
+
+            x+=orig_x
+
+            tf.logging.debug('image after unit %s' ,x.get_shape())
+            return x
+    def _decay(self):
+        costs=[]
+        for var in tf.trainable_variables():
+            if var.op.name.find(r'conv_w') > 0:
+                print var.op.name
+                costs.append(tf.nn.l2_loss(var))
+            if var.op.name.find(r'fc_w') > 0:
+                print var.op.name
+                costs.append(tf.nn.l2_loss(var))
+
+        return tf.multiply(self.hps.weight_decay_rate , tf.add_n(costs))
+
+    def _build_model(self):
+        with tf.variable_scope('init'):
+            x_ = self._images
+            x_ = self._conv('init_conv' , x_ , 3 ,3 ,16 , self. _stride(1))
+            strides = [1, 2, 2]
+            activate_before_residual = [True, False, False]
+
+            if self.hps.use_bottleneck:
+                res_func = self._bottlenect_residual
+                filters= [16, 64,128 ,256]
+
+            else:
+                res_func = self._residual
+                filters = [16,16,32,64]
+
+
+        with tf.variable_scope('unit_1'):
+            x_= res_func(x_,filters[0] , filters[1] ,self._stride(strides[0]) , activate_before_residual[0])
+        for i in six.moves.range(1  , self.hps.n_residual_units):
+
+            with tf.variable_scope('unit_1_%d' % i):
+                x_=res_func(x_ , filters[1] , filters[1] , self. _stride(1) , False)
+        print 'unit_1 end:' ,x_.get_shape()
+        print '----------------------------'
+        with tf.variable_scope('unit_2'):
+            x_ = res_func(x_ , filters[1] , filters[2] , self._stride(strides[1]) ,activate_before_residual[1])
+        print x_.get_shape()
+        for i in six.moves.range(1, self.hps.n_residual_units):
+            with tf.variable_scope('unit_2_%d' % i):
+                x_=res_func(x_, filters[2] , filters[2] ,self._stride(1) , False)
+        print 'unit_2 end:' ,x_.get_shape()
+        print '----------------------------'
+        with tf.variable_scope('unit_3'):
+            x_ = res_func(x_ , filters[2] , filters[3] , self._stride(strides[2]) , activate_before_residual[2])
+        for i in six.moves.range(1, self.hps.n_residual_units):
+            with tf.variable_scope('unit_3_%d' % i):
+                x_=res_func(x_ , filters[3] , filters[3] , self._stride(1) , False)
+        print 'unit_3 end:', x_.get_shape()
+        print '----------------------------'
+
+        with tf.variable_scope('unit_4'):
+            x_ = res_func(x_ , filters[3] , 128 , self._stride(1) , False)
+            print 'unit_4 end:', x_.get_shape()
+        with tf.variable_scope('unit_4_1'):
+            x_ = res_func(x_, 128, 128, self._stride(1), False)
+            print 'unit_4_1 end:', x_.get_shape()
+        with tf.variable_scope('unit_4_2'):
+            x_ = res_func(x_, 128, 128, self._stride(1), False)
+            print 'unit_4_2 end:', x_.get_shape()
+
+        print 'unit_4 end:', x_.get_shape()
+        print '----------------------------'
+
+        with tf.variable_scope('unit_last'):
+            #x_ = res_func(x_, filters[3], filters[3], self._stride(1), False)
+            #x_ = res_func(x_, filters[3], filters[3], self._stride(1), False)
+            x_ = self._batch_norm('final_bn' , x_ )
+            x_ = self._act(x_ ,actmode='leaky_relu')
+            x_ = self._gap(x_)
+        print 'unit_last end:', x_.get_shape()
+        print '----------------------------'
+
+        with tf.variable_scope('logits'):
+            logits = self._affine(x_ , self.hps.n_classes)
+            self.predictions  = tf.nn.softmax(logits)
+        with tf.variable_scope('cost'):
+            xent = tf.nn.softmax_cross_entropy_with_logits(logits=logits , labels=self.label)
+            self.cost = tf.reduce_mean(xent,name='xent')
+            self.cost += self._decay()
+
+            tf.summary.scalar('cost',self.cost)
+
+
+    def _build_train_op(self):
+        """
+
+        :return:
+        """
+        self.lrn_rate =tf.constant(self.hps.lrn_rate , tf.float32)
+        tf.summary.scalar('learning_rate', self.lrn_rate )
+
+        trainable_variable= tf.trainable_variables()
+        grads = tf.gradients(self.cost ,trainable_variable)
+
+        if self.hps.optimizer== 'sgd':
+            optimizer = tf.train.GradientDescentOptimizer(self.lrn_rate)
+        elif self.hps.optimizer=='mom':
+            optimizer = tf.train.MomentumOptimizer(self.lrn_rate , 0.9)
+
+        apply_op = optimizer.apply_gradients(zip(grads , trainable_variable) , global_step=self.global_step , name='train_step')
+        train_ops = [apply_op] + self._extra_train_ops
+        self.train_op = tf.group(*train_ops)
+
+    def build_graph(self):
+        """using thie function you can build more detail model"""
+        """model arcitecture"""
+        with tf.variable_scope('init'):
+            """ in this line , start model"""
+        x_ = self._images
+        self.global_step = tf.contrib.framework.get_or_create_global_step()
+        self._build_model()
+        if self.mode == 'train':
+            self._build_train_op()
+        self.summaries = tf.summary.merge_all()
+
+
+
+
+
