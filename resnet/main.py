d3c7db767fe11f644393c787bced805fcf166530
kimseongjung <plznw4me@naver.com>
2017-09-14 21:07:33 +0900

    add mnist data set

--- resnet/main.py
+++ resnet/main.py
@@ -118,7 +118,7 @@ def train(hps):
 
 
         if i%check_point:
-            sess.run(precision, feed_dict={x_: batch_xs, y_: batch_ys})
+            print sess.run(precision, feed_dict={x_: batch_xs, y_: batch_ys})
         sess.run(cls_resnet.train_op, feed_dict={x_: batch_xs, y_: batch_ys})
 
 
